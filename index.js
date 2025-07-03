451daf635a066a5ce4770a49bb87f8cfad229782
Sygris <tiago.a.vista@gmail.com>
2025-07-03 19:27:21 +0100

    Starting to code grid generation

--- index.js
+++ index.js
@@ -13,6 +13,28 @@ buttonsList.forEach(button => button.addEventListener("click", () => {
   setActive(button);
 }));
 
-function generateGrid() {
 
+const grid = document.querySelector("#grid");
+let gridSize = 16;
+
+grid.style.display = "grid";
+
+document.querySelector('#gridSize').addEventListener("input", (e) => {
+  const output = document.querySelector("#sizeValue");
+  gridSize = e.target.value;
+  output.textContent = gridSize + " x " + gridSize;
+  generateGrid()
+});
+
+function clearGrid() {
+  grid.innerHTML = "";
+}
+
+function generateGrid() {
+  clearGrid();
+  for (let i = 0; i < gridSize; i++) {
+    let newDiv = document.createElement(("div"));
+    newDiv.style.background = "Red";
+    grid.appendChild(newDiv);
+  }
 }
