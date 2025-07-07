cb32cc4e88712730b08087176d7bd2d573f776e7
Sygris <tiago.a.vista@gmail.com>
2025-07-07 21:25:44 +0100

    Grid has now borders and it´s being fully generated using the slider for size

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const DEFAULT_SIZE = 16;
+let gridSize = DEFAULT_SIZE;
+
 function setActive(button) {
   if (currentActiveButton != null) {
     currentActiveButton.classList.remove("active");
@@ -13,16 +16,18 @@ buttonsList.forEach(button => button.addEventListener("click", () => {
   setActive(button);
 }));
 
+const clearButton = document.querySelector("#clear");
+clearButton.addEventListener("click", clearButton);
 
 const grid = document.querySelector("#grid");
-let gridSize = 16;
 
 document.querySelector('#gridSize').addEventListener("input", (e) => {
   const output = document.querySelector("#sizeValue");
   gridSize = e.target.value;
   output.textContent = gridSize + " x " + gridSize;
-  generateGrid()
-});
+
+  generateGrid();
+}); 
 
 function clearGrid() {
   grid.innerHTML = "";
@@ -33,10 +38,15 @@ function generateGrid() {
 
   grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
   grid.style.gridTemplaterows = `repeat(${gridSize}, 1fr)`;
-
+  
   for (let i = 0; i < gridSize * gridSize; i++) {
     let newDiv = document.createElement(("div"));
-    newDiv.style.background = "Red";
+    newDiv.classList.add("grid-element");
     grid.appendChild(newDiv);
   }
 }
+
+window.onload = (event) => {
+  console.log("TEST");
+  generateGrid();
+};
